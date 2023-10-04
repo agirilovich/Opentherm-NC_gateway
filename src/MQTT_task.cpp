470e64c741d9e0367d734ec9e0896466113556d1
agirilovich <agirilovich@gmail.com>
2023-10-05 00:17:15 +0200

    first mqtt working version

---
+++ src/MQTT_task.cpp
@@ -0,0 +1,173 @@
+#include "MQTT_task.h"
+#include "controlWiFi.h"
+#include "Credentials.h"
+
+//Define MQTT Topic for HomeAssistant Discovery
+const char *MQTTTSetTopicConfig = MQTT_TOPIC_TSET_CONFIG;
+const char *MQTTchEnableTopicConfig = MQTT_TOPIC_CHENABLE_CONFIG;
+const char *MQTTMaxRelModLevelSettingTopicConfig = MQTT_TOPIC_MAXMODLEVEL_CONFIG;
+const char *MQTTTrSetTopicConfig = MQTT_TOPIC_TRSET_CONFIG;
+const char *MQTTTrTopicConfig = MQTT_TOPIC_ROOMTEMP_CONFIG;
+
+
+//Define MQTT Topic for HomeAssistant Sensor state
+const char *MQTTTSetTopicState = MQTT_TSET_TOPIC_STATE;
+const char *MQTTchEnableTopicState = MQTT_CHENABLE_TOPIC_STATE;
+const char *MQTTMaxRelModLevelSettingTopicState = MQTT_MAXMODLEVEL_TOPIC_STATE;
+const char *MQTTTrSetTopicState = MQTT_TRSET_TOPIC_STATE;
+const char *MQTTTrTopicState = MQTT_ROOMTEMP_TOPIC_STATE;
+
+const char *mqtt_host = mqtt_server;
+const int mqtt_port = 1883;
+const char *mqtt_user = mqtt_username;
+const char *mqtt_pass = mqtt_password;
+
+//Define objects for MQTT messages in JSON format
+#include <ArduinoJson.h>
+StaticJsonDocument<512> JsonSensorConfig;
+char Buffer[1024];
+
+WiFiClient client;
+
+#include <PubSubClient.h>
+PubSubClient mqtt(client);
+
+
+void initMQTT()
+{
+  Serial.print("Connecting to MQTT broker host: ");
+  Serial.println(mqtt_host);
+  while (!client.connect(mqtt_host, mqtt_port))
+  {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("Connected!");
+
+  //Initialise MQTT autodiscovery topic and sensor
+  mqtt.setServer(mqtt_host, mqtt_port);
+
+  Serial.print("Testing connection to mqtt broker...");
+  while (!mqtt.connect(DEVICE_BOARD_NAME, mqtt_user, mqtt_pass))
+  {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  if (mqtt.connected()) {
+    Serial.println(" connected!");
+  } 
+
+  //CH water temp. setpoint
+  JsonSensorConfig["name"] = "CH water temp. setpoint";
+  JsonSensorConfig["device_class"] = "temperature";
+  JsonSensorConfig["state_class"] = "measurement";
+  JsonSensorConfig["unit_of_measurement"] = "C";
+  JsonSensorConfig["uniq_id"] = "chwatersetpointC";
+  JsonSensorConfig["state_topic"] = MQTTTSetTopicState;
+
+  JsonObject device  = JsonSensorConfig.createNestedObject("device");
+  device["identifiers"][0] = "openthermgw000";
+  device["connections"][0][0] = "mac";
+  device["connections"][0][1] = "08:B6:1F:33:7D:AC";
+  device["model"] = "OP-GW-01";
+  device["name"] = SENSOR_NAME;
+  device["manufacturer"] = "Aliexpress"; 
+  device["sw_version"] = "1.0";  
+  serializeJson(JsonSensorConfig, Buffer);
+  initializeMQTTTopic(MQTTTSetTopicConfig, Buffer);
+
+  //CH active sensor
+  JsonSensorConfig["name"] = "CH active";
+  JsonSensorConfig["device_class"] = "heat";
+  JsonSensorConfig["state_class"] = "";
+  JsonSensorConfig["unit_of_measurement"] = "";
+  JsonSensorConfig["uniq_id"] = "chactivestate";
+  JsonSensorConfig["state_topic"] = MQTTchEnableTopicState;
+
+  serializeJson(JsonSensorConfig, Buffer);
+  initializeMQTTTopic(MQTTchEnableTopicConfig, Buffer);
+
+  //Maximum relative modulation level setting
+  JsonSensorConfig["name"] = "Max modulation level";
+  JsonSensorConfig["device_class"] = "power_factor";
+  JsonSensorConfig["state_class"] = "measurement";
+  JsonSensorConfig["unit_of_measurement"] = "%";
+  JsonSensorConfig["uniq_id"] = "chmaxmodulationlvl";
+  JsonSensorConfig["state_topic"] = MQTTMaxRelModLevelSettingTopicState;
+
+  serializeJson(JsonSensorConfig, Buffer);
+  initializeMQTTTopic(MQTTMaxRelModLevelSettingTopicConfig, Buffer);
+
+  //Room Setpoint sensor
+  JsonSensorConfig["name"] = "Room setpoint";
+  JsonSensorConfig["device_class"] = "temperature";
+  JsonSensorConfig["state_class"] = "measurement";
+  JsonSensorConfig["unit_of_measurement"] = "C";
+  JsonSensorConfig["uniq_id"] = "chroomsetpoint";
+  JsonSensorConfig["state_topic"] = MQTTTrSetTopicState;
+
+  serializeJson(JsonSensorConfig, Buffer);
+  initializeMQTTTopic(MQTTTrSetTopicConfig, Buffer);
+
+  //Room Temperature
+  JsonSensorConfig["name"] = "Room Temperature";
+  JsonSensorConfig["device_class"] = "temperature";
+  JsonSensorConfig["state_class"] = "measurement";
+  JsonSensorConfig["unit_of_measurement"] = "C";
+  JsonSensorConfig["uniq_id"] = "chroomtemperature";
+  JsonSensorConfig["state_topic"] = MQTTTrTopicState;
+
+  serializeJson(JsonSensorConfig, Buffer);
+  initializeMQTTTopic(MQTTTrTopicConfig, Buffer);
+
+}
+
+void initializeMQTTTopic(const char *Topic, char *SensorConfig)
+{
+  
+  Serial.println("Initialise MQTT autodiscovery topics and sensors...");
+  Serial.println(Topic);
+  //Serial.println(SensorConfig);
+
+  //Publish message to AutoDiscovery topic
+  if (mqtt.publish(Topic, SensorConfig, true)) {
+    Serial.println("Done");
+  }
+    
+  //Gracefully close connection to MQTT broker
+}
+
+void MQTTMessageCallback(float SetPoint, bool CHActive, float MaxModulationLevel, float RoomSetPoint, float RoomTemperature)
+{
+  char MessageBuf[16];
+  //Publish MQTT messages
+  Serial.println("Publishing MQTT messages...");
+  mqtt.connect(DEVICE_BOARD_NAME, mqtt_user, mqtt_pass);
+  if (mqtt.connected()) {
+
+    sprintf(MessageBuf, "%d", int(SetPoint));
+    mqtt.publish(MQTTTSetTopicState, MessageBuf, false);
+
+    sprintf(MessageBuf, "%s", CHActive?"ON":"OFF");
+    mqtt.publish(MQTTchEnableTopicState, MessageBuf, false);
+
+    sprintf(MessageBuf, "%d", int(MaxModulationLevel));
+    mqtt.publish(MQTTMaxRelModLevelSettingTopicState, MessageBuf, false);
+
+    sprintf(MessageBuf, "%d", int(RoomSetPoint));
+    mqtt.publish(MQTTTrSetTopicState, MessageBuf, false);
+
+    sprintf(MessageBuf, "%d", int(RoomTemperature));
+    mqtt.publish(MQTTTrTopicState, MessageBuf, false);
+  
+  }
+  else {
+    Serial.println("Unable to connect to MQTT broker");
+    Serial.println("Cycle is skipped");
+    Serial.print("Number of failed attempts in a chain: ");
+  }
+  mqtt.disconnect();
+  Serial.println("Done");
+}
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,31 @@
 #include <Arduino.h>
 #include <OpenTherm.h>
 
+#ifndef DEVICE_BOARD_NAME
+#  define DEVICE_BOARD_NAME "OpenthermSlave"
+#endif
+
+#define DEVICE_HOSTNAME "OThermGW"
+
+#include "controlWiFi.h"
+
+#include "MQTT_task.h"
+
+#include <esp_task_wdt.h>
+#define WDT_TIMEOUT 60
+
+#define LED_BUILTIN 22
+
+//Global variables in Master<>Slave communication
+float SetPoint = 0;
+bool CHActive = false;
+float MaxModulationLevel = 0;
+float RoomSetPoint = 0;
+float RoomTemperature = 0;
+
+hw_timer_t *Timer0_Cfg = NULL;
+
+
 const int inPin = 19;  //for Arduino, 12 for ESP8266 (D6), 19 for ESP32
 const int outPin = 23; //for Arduino, 13 for ESP8266 (D7), 23 for ESP32
 OpenTherm ot(inPin, outPin, true);
@@ -9,8 +34,14 @@ void IRAM_ATTR handleInterrupt() {
     ot.handleInterrupt();
 }
 
+bool publishMQTT = false;
+void IRAM_ATTR Timer0_ISR()
+{
+  publishMQTT = true;
+}
+
 void processRequest(unsigned long request, OpenThermResponseStatus status) {
-    Serial.println("T" + String(request, HEX)); //master/thermostat request
+    //Serial.println("T" + String(request, HEX)); //master/thermostat request
 
     unsigned long response = 0;
     OpenThermMessageID id = ot.getDataID(request);
@@ -18,58 +49,264 @@ void processRequest(unsigned long request, OpenThermResponseStatus status) {
     float f = ot.getFloat(request);
     switch(id)
     {
+      //message ID 0 Master and slave status flags
       case OpenThermMessageID::Status:
       {
+        Serial.println("Master/Slave status flags");
+        Serial.println("Received data: " + String(data));
         uint8_t statusRequest = data >> 8;
+        Serial.println("statusRequest: " + String(statusRequest));
         uint8_t chEnable = statusRequest & 0x1;
-        uint8_t dhwEnable = statusRequest & 0x2;
+        Serial.println("chEnable: " + String(chEnable));
         data &= 0xFF00;
-        //data |= 0x01; //fault indication
         if (chEnable) data |= 0x02; //CH active
-        if (dhwEnable) data |= 0x04; //DHW active
-        if (chEnable || dhwEnable) data |= 0x08; //flame on
-        //data |= 0x10; //cooling active
-        //data |= 0x20; //CH2 active
-        //data |= 0x40; //diagnostic/service event
-        //data |= 0x80; //electricity production on
-
+        if (chEnable) data |= 0x08; //flame on
         response = ot.buildResponse(OpenThermMessageType::READ_ACK, id, data);
+        Serial.println("Responded data: " + String(data));
+        if (chEnable) {
+          CHActive = true;
+        }
         break;
       }
+
+      //message ID 1 Control setpoint ie CH water temp. setpoint
       case OpenThermMessageID::TSet:
       {
+        Serial.println("Gotten control setpoint: " + String(f));
+        SetPoint = f;
         response = ot.buildResponse(OpenThermMessageType::WRITE_ACK, id, data);
         break;
       }
+
+      //message ID 2 HB: Master configuration
+      case OpenThermMessageID::MConfigMMemberIDcode:
+      {
+        data &= 0xFF00;
+        response = ot.buildResponse(OpenThermMessageType::WRITE_ACK, id, data);
+        break;
+      }
+
+      //message ID 3 Slave configuration flags
+      case OpenThermMessageID::SConfigSMemberIDcode:
+      {
+        Serial.print("Slave configuration flags: ");
+        data &= 0xFF00;
+        //data |= 0x01; //DHW Present
+        data |= 0x02; //Control Type
+        response = ot.buildResponse(OpenThermMessageType::READ_ACK, id, data);
+        Serial.println(String(data));
+        break;
+      }
+
+      //message ID 5 HB: Application-specific fault flags
+      case OpenThermMessageID::ASFflags:
+      {
+        data &= 0xFF00;
+        response = ot.buildResponse(OpenThermMessageType::READ_ACK, id, data);
+        break;
+      }
+
+      //message ID 6 HB: Remote-parameter transfer-enable flags
+      case OpenThermMessageID::RBPflags:
+      {
+        data &= 0xFF00;
+        response = ot.buildResponse(OpenThermMessageType::READ_ACK, id, data);
+        break;
+      }
+
+
+      //message ID 9 Remote override room setpoint
+      case OpenThermMessageID::TrOverride:
+      {
+        data = ot.temperatureToData(0);
+        response = ot.buildResponse(OpenThermMessageType::READ_ACK, id, data);
+        break;
+      }
+
+      //message ID 12  HB: Size of Fault Buffer
+      case OpenThermMessageID::FHBsize:
+      {
+        response = ot.buildResponse(OpenThermMessageType::READ_ACK, id, 0);
+        break;
+      }
+
+      //message ID 14 Maximum relative modulation level setting
+      case OpenThermMessageID::MaxRelModLevelSetting:
+      {
+        MaxModulationLevel = f;
+        response = ot.buildResponse(OpenThermMessageType::WRITE_ACK, id, data);
+        break;
+      }
+
+      //message ID 16  Room Setpoint
+      case OpenThermMessageID::TrSet:
+      {
+        RoomSetPoint = f;
+        response = ot.buildResponse(OpenThermMessageType::WRITE_ACK, id, data);
+        break;
+      }
+
+      //message ID 17 Relative modulation lev
+      case OpenThermMessageID::RelModLevel:
+      {
+        float modulation = 50;
+        Serial.println("Requested Current Modulation Level");
+        Serial.println("Sent constant " + String(modulation) + " %");
+        data = ot.temperatureToData(modulation);
+        response = ot.buildResponse(OpenThermMessageType::READ_ACK, id, data);
+        break;
+      }
+
+      //message ID 18 CH water pressure
+      case OpenThermMessageID::CHPressure:
+      {
+        float pressure = 1;
+        Serial.println("Requested  CH water pressure");
+        Serial.println("Sent constant " + String(pressure) + " bar");
+        data = ot.temperatureToData(pressure);
+        response = ot.buildResponse(OpenThermMessageType::READ_ACK, id, data);
+        break;
+      }
+
+      //message ID 19 DHW flow rate
+      case OpenThermMessageID::DHWFlowRate:
+      {
+        Serial.println("Requested DHW flow rate");
+        data = ot.temperatureToData(0);
+        response = ot.buildResponse(OpenThermMessageType::READ_ACK, id, data);
+        Serial.println("Sent constant " + 0);
+        break;
+      }
+
+      //message ID 24 Room Temperature
+      case OpenThermMessageID::Tr:
+      {
+        Serial.println("Got room temperature " + String(f));
+        RoomTemperature = f;
+        response = ot.buildResponse(OpenThermMessageType::WRITE_ACK, id, data);
+        break;
+      }
+
+      //message ID 25 Boiler temperature
       case OpenThermMessageID::Tboiler:
       {
         data = ot.temperatureToData(45);
         response = ot.buildResponse(OpenThermMessageType::READ_ACK, id, data);
         break;
       }
+
+      //message ID 26 DHW temperature
+      case OpenThermMessageID::Tdhw:
+      {
+        float DHWTemperature = 20;
+        data = ot.temperatureToData(DHWTemperature);
+        Serial.println("Requested  DHW temperature");
+        Serial.println("Sent constant " + String(DHWTemperature) + " C");
+        response = ot.buildResponse(OpenThermMessageType::READ_ACK, id, data);
+        break;
+      }
+
+      //message ID 27 Outside temperature
+      case OpenThermMessageID::Toutside:
+      {
+        float OutsideTemperature = 10;
+        data = ot.temperatureToData(OutsideTemperature);
+        Serial.println("Requested  outside temperature");
+        Serial.println("Sent constant " + String(OutsideTemperature) + " C");
+        response = ot.buildResponse(OpenThermMessageType::READ_ACK, id, data);
+        break;
+      }
+
+      //message ID 28 Return water temperature
+      case OpenThermMessageID::Tret:
+      {
+        float ReturnTemperature = 35;
+        data = ot.temperatureToData(ReturnTemperature);
+        Serial.println("Requested  return boiler temperature");
+        Serial.println("Sent constant " + String(ReturnTemperature) + " C");
+        response = ot.buildResponse(OpenThermMessageType::READ_ACK, id, data);
+        break;
+      }
+
       default:
       {
+        Serial.println("Undefined MessageID: " + String(id));
+        //Serial.println("data" + String(data));
+        //Serial.println("f" + String(f));
         //build UNKNOWN-DATAID response
         response = ot.buildResponse(OpenThermMessageType::UNKNOWN_DATA_ID, ot.getDataID(request), 0);   
       }
     }
-    Serial.println("B" + String(response, HEX)); //slave/boiler response
+    //Serial.println("B" + String(response, HEX)); //slave/boiler response
 
-    //send response
+    Serial.println("-------------------------------");
     delay(20); //20..400ms, usually 100ms
     ot.sendResponse(response);
 }
 
 void setup()
 {
-    Serial.begin(9600);
-    Serial.println("Start");
+  Serial.begin(9600);
+  Serial.println("Start");
 
-    ot.begin(handleInterrupt, processRequest);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  esp_reset_reason_t reason = esp_reset_reason();
+  switch (reason) {
+    case ESP_RST_POWERON:
+      Serial.println("Reset due to power-on event");
+    break;
+  
+    case ESP_RST_SW:
+      Serial.println("Software reset via esp_restart");
+    break;
+
+    case ESP_RST_WDT:
+      Serial.println("Rebooted by Watchdog!");
+    break;
+  }
+
+  //Set witchdog timeout for 32 seconds
+  esp_task_wdt_init(WDT_TIMEOUT, true);
+  esp_task_wdt_add(NULL);
+
+  while (esp_task_wdt_status(NULL) != ESP_OK) {
+    // LED blinks indefinitely
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+  }
+
+  Serial.print("Start WiFi on ");
+  Serial.println(DEVICE_BOARD_NAME);
+  
+  initializeWiFi(DEVICE_HOSTNAME);
+  
+  establishWiFi();
+
+  // you're connected now, so print out the data
+  printWifiStatus();
+
+  initMQTT();
+
+  //Setup Hardware Timer for MQTT publish
+  Timer0_Cfg = timerBegin(0, 200, true);
+  timerAttachInterrupt(Timer0_Cfg, &Timer0_ISR, true);
+  timerAlarmWrite(Timer0_Cfg, 10000000, true);
+  timerAlarmEnable(Timer0_Cfg);
+
+  ot.begin(handleInterrupt, processRequest);
 }
 
 
 void loop()
 {
-    ot.process();
+  ot.process();
+  if (publishMQTT)
+  {
+    MQTTMessageCallback(SetPoint, CHActive, MaxModulationLevel, RoomSetPoint, RoomTemperature);
+    publishMQTT = false;
+  }
+  esp_task_wdt_reset();
 }
